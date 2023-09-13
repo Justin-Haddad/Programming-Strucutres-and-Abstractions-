e3ca7a20cbbe5c0fdffd5181d834a975720322d7
TheJusBus <jbhaddad03@gmail.com>
2023-09-13 01:40:00 -0500

    First Commit

---
+++ Java1Review.java
@@ -0,0 +1,128 @@
+import java.util.Arrays;
+
+/**
+ * Implement each of the 10 methods tested in JUnitTests.java. Study the tests
+ * to determine how the methods should work.
+ */
+public class Java1Review {
+	
+	public static double divide(double x, double y) 
+	{
+		return x/y;
+	}
+	
+	public static int divide(int x, int y)
+	{
+		return x/y;
+	}
+	
+	public static boolean isDivisibleBy7(int num)
+	{
+		if ((num % 7) == 0)
+		{
+			return true;
+		}
+		else 
+		{
+			return false;
+		}
+
+	}
+	
+	public static String main(String input)
+	{
+		return "Overloaded main method was passed \"" + input + "\".";
+		
+	}
+	
+	public static int findMin(int a, int b, int c)
+	{
+		if ((a < b) && (a < c))
+		{
+			return a;
+		}
+		else if ((b < c) && (b < a))
+		{
+			return b;
+		}
+		else if ((c < b) && (c < a))
+		{
+			return c;
+		}
+		return 0;
+		
+	}
+
+	 	public static int findMin(int[] array)
+	 	{
+	 		Arrays.sort(array);
+	 		return array[0];
+	 	}
+	 	
+	 	
+		 public static void removeDuplicates(int[] array)
+		 {
+			 int num = 0;
+			 for(int index =0; index < array.length; index++)
+			 {
+				 boolean isDuplicate = false;
+				 num = array[index];
+				 for( int i = index + 1; i < array.length; i++)
+				 {
+					 if(num == array[i])
+					 {
+						 isDuplicate = true;
+						 array[i] = 0;
+					 }
+					 if (isDuplicate)
+					 {
+						 array[index] = 0;
+					 }
+			 }
+		 }
+		 }
+	 	
+	 	public static double average(int[] array)
+	 	{
+	 		double total = 0;
+	 		for (int index = 0; index < array.length; index++)
+	 		{
+	 			total = total + array[index];
+	 		}
+	 		double average = total / array.length;
+	 		
+	 		return average;
+	 	}
+	 	
+	 	public static void toLowerCase(String[] array)
+	 	{
+	 		for (int index = 0; index < array.length; index++)
+	 		{
+	 			array[index] = array[index].toLowerCase();
+	 		}
+	 		
+	 	}
+	 	
+	 	public static String[] toLowerCaseCopy(String[] array)
+	 	{
+	 		String[] phrase;
+	 		phrase = Arrays.copyOf(array, array.length);
+	 		for (int index= 0; index < phrase.length; index++)
+	 		{
+	 			phrase[index] = phrase[index].toLowerCase();
+	 		}
+	 		return phrase;
+	 	}
+	 	
+	 	
+	 
+	
+	public static void main(String[] args) {
+		// If you want to write your own tests, do so here. (Do not modify
+		// JUnitTests.java.) To run this method in Eclipse, right-click
+		// Java1Review.java in the Package Explorer and select "Run As" >
+		// "Java Application" from the context menu.
+		
+	}
+
+}
